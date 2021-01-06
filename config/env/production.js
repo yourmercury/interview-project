cf0e8b5966636e24fc91a229f33248109663f0b7
mercury <momohpolycarp@gmail.com>
2021-01-06 08:15:48 +0100

    7th comit

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,7 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    onlyAllowOrigins: [
-      'https://interview-project.herokuapp.com/',
-      'https://interview-project.herokuapp.com/',
-    ],
+    onlyAllowOrigins: 'https://interview-project.herokuapp.com/',
 
 
     /***************************************************************************
